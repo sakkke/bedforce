d63fea39c741cac7da16e683764b7cb55f895bb4
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-25 12:53:51 +0900

    feat: add 'bedforce' command

--- src/main/kotlin/dev/sakkke/bedforce/Bedforce.kt
+++ src/main/kotlin/dev/sakkke/bedforce/Bedforce.kt
@@ -1,9 +1,41 @@
 package dev.sakkke.bedforce
 
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class Bedforce: JavaPlugin() {
     override fun onEnable() {
-        println("hello world")
+        logger.info("Bedforce enabled")
+    }
+
+    override fun onCommand(sender: CommandSender, cmd: Command, label: String, args: Array<String>): Boolean {
+        if (cmd.name == "bedforce") {
+            if (sender is Player) {
+                val inventory = sender.inventory
+                val hasBed = inventory.contents.any { itemStack ->
+                    itemStack?.type.toString().endsWith("_BED")
+                }
+
+                if (hasBed) {
+                    if (sender.world.time >= 13000) {
+                        sender.sendMessage("Changing the time to day")
+                        sender.world.time = 1000
+                    } else {
+                        sender.sendMessage("It's not night")
+                    }
+                } else {
+                    sender.sendMessage("You need a bed in your inventory")
+                }
+            } else {
+                sender.sendMessage("Changing the time to day in all worlds")
+                server.worlds.forEach { world ->
+                    world.time = 1000
+                }
+            }
+            return true
+        }
+        return false
     }
 }
