6180f6f8c317bb26c6c518ef00e133fb0f7f5764
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-25 11:22:33 +0900

    feat: add 'hello world'

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,19 +2,28 @@ plugins {
     kotlin("jvm") version "1.9.22"
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "dev.sakkke"
+version = "0.1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven(url = "https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
 }
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    compileOnly("org.spigotmc:spigot-api:1.20.1-R0.1-SNAPSHOT")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    from({
+        configurations.runtimeClasspath.get().filter { it.isDirectory }.map { it }
+        configurations.runtimeClasspath.get().filter { !it.isDirectory }.map { zipTree(it) }
+    })
 }
 kotlin {
     jvmToolchain(17)
---
+++ src/main/kotlin/dev/sakkke/bedforce/Bedforce.kt
@@ -0,0 +1,9 @@
+package dev.sakkke.bedforce
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Bedforce: JavaPlugin() {
+    override fun onEnable() {
+        println("hello world")
+    }
+}
